6d5a856e2579997ddfbf1bc7a4c5a71791332ecf
Vladimir Kovalyov <frontx25@gmail.com>
2020-04-20 07:45:27 +0300

    разработка первоначального дизайна

--- src/App.js
+++ src/App.js
@@ -1,19 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+const App = () => {
+    return (
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcToJ7QR-40ysomI8GKEjbHqdvd4bNfBpgKsuwqMdV0WogtMshuL&usqp=CAU"
+                    alt=""/>
+            </header>
+            <nav className='nav'>
+                <div><a>Profile</a></div>
+                <div><a>Messages</a></div>
+                <div><a>News</a></div>
+                <div><a>Music</a></div>
+                <div><a>Settings</a></div>
+            </nav>
+            <div className='content'>
+                <div>
+                    <img
+                        src="https://www.wur.nl/upload_mm/f/3/0/63b287aa-f976-4a3d-baed-c2035f14dd8f_Irene_tinto-river.jpg"
+                        alt=""/>
+                </div>
+                <div>
+                    avatar+description
+                </div>
 
-      </header>
-    </div>
-  );
+                <div>
+                    my post
+                    <div>
+                        new post
+                    </div>
+                </div>
+
+                <div>
+                    post1
+                </div>
+                <div>
+                    post2
+                </div>
+
+
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
