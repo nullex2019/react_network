c8e96130ae420d2d9512c51b6c6afd43cf481c37
Vladimir Kovalyov <frontx25@gmail.com>
2020-06-02 17:49:40 +0300

    pagination, постраничный вывод пользователей

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -51,21 +51,44 @@ import userPhoto from '../../assets/images/user.png'
 class Users extends React.Component {
 
     componentDidMount() {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items);
+                this.props.setTotalUserCount(response.data.totalCount);
+            })
+    }
+//обработчик при нажатии (метод)
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
             .then(response => {
                 this.props.setUsers(response.data.items)
             })
     }
-
     render() {
 
-        return (
+        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);//округляем
+        let pages = [];
+        for (let i = 1; i <= pagesCount; i++) {
+            pages.push(i)
+        }
 
-            <div className={s.container}>
+
+        return (<div className={s.container}>
+                <div>
+
+                    {pages.map(p => {return <span
+                        className={this.props.currentPage === p && s.selectedPage}
+                        onClick={(event) => {this.onPageChanged(p)}}>
+                            {p}
+                        </span>
+                    })}
+                </div>
                 {this.props.users.map(u => <div key={u.id}>
             <span>
                 <div>
-                    <img src={u.photos.small != null ? u.photos.small : userPhoto} alt="ava" className={s.userPhoto}/>
+                    <img src={u.photos.small != null ? u.photos.small : userPhoto}
+                         alt="ava" className={s.userPhoto}/>
                 </div>
                 <div>
                     {u.followed
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -1,10 +1,15 @@
 const FOLLOW = 'FOLLOW';
 const UNFOLLOW = "UNFOLLOW";
 const SET_USERS = "SET_USERS";
+const SET_CURRENT_PAGE = "SET_CURRENT_PAGE";
+const SET_TOTAL_USERS_COUNT = "SET_TOTAL_USERS_COUNT";
 
 
 let initialState = {
     users: [],
+    pageSize: 100,
+    totalUsersCount:0,
+    currentPage:1 //текущая страница сервака
 }
 
 const usersReducer = (state = initialState, action) => {
@@ -31,8 +36,13 @@ const usersReducer = (state = initialState, action) => {
                 })
             }
         case SET_USERS: {
-
-            return {...state, users: [...state.users, ...action.users]}
+            return {...state, users: action.users}
+        }
+        case SET_CURRENT_PAGE: {
+            return {...state, currentPage: action.currentPage}
+        }
+        case SET_TOTAL_USERS_COUNT: {
+            return {...state, totalUsersCount: action.count}
         }
         default:
             return state
@@ -42,5 +52,7 @@ const usersReducer = (state = initialState, action) => {
 export const followAC = (userId) => ({type: FOLLOW, userId})
 export const unfollowAC = (userId) => ({type: UNFOLLOW, userId})
 export const setUsersAC = (users) => ({type: SET_USERS, users}) //Берем юзеров из сервака
+export const setCurrentPageAC = (currentPage) => ({type: SET_CURRENT_PAGE, currentPage}) //Берем юзеров из сервака
+export const setTotalUserCountAC = (totalUsersCount) => ({type: SET_TOTAL_USERS_COUNT, count: totalUsersCount}) //Установить общее количество пользователей
 
 export default usersReducer;
