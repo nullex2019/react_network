10597d2927b6bdf6e4299060933e81ecf93364ad
Vladimir Kovalyov <frontx25@gmail.com>
2020-06-06 19:12:54 +0300

    Практика, DAL, axios.create

---
+++ src/api/api.js
@@ -0,0 +1,23 @@
+import * as axios from "axios";
+
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers: {
+        "API-KEY": "225c838c-fc40-4ad6-a1f3-3843b2819080"
+    }
+
+})
+
+export const userAPI = {
+    getUsers(currentPage, pageSize) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+            .then(response => {
+                return response.data;
+            });
+    }
+}
+
+
+
